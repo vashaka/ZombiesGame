5839ee4c391ad2a6edef3fb78c637ec1ff3ee7c9
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2022-10-11 14:36:22 +0400

    weapon logic from mac

---
+++ Zombies/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitpoints = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        hitpoints -= damage;
+        if(hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Zombies/Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f; 
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            print("I hit this thing " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        } else {
+            return;
+        }
+    }
+}
